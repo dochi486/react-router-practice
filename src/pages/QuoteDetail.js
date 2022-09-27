6e69edf7ef66b1b1fdd7c49e778f3a0fd18df1b5
dochi486 <93mskim@gmail.com>
2022-09-27 18:59:49 +0900

    인용문 상세 페이지 연결 작업

--- src/pages/QuoteDetail.js
+++ src/pages/QuoteDetail.js
@@ -1,13 +1,31 @@
 import { useParams, Route } from 'react-router-dom';
 import Comments from '../components/comments/Comments';
+import HighlightedQuote from '../components/quotes/HighlightedQuote';
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: '붱키',
+    text: '리액트는 재미없엉',
+  },
+  {
+    id: 'q2',
+    author: '준키',
+    text: '코딩은 재미없엉',
+  },
+];
 
 const QuoteDetail = () => {
   const params = useParams();
 
+  const quote = DUMMY_QUOTES.find((quote) => quote.id === params.quoteId);
+
+  if (!quote) {
+    return <p>No quote found!</p>;
+  }
+
   return (
     <>
-      <h1>Quote Detail Page</h1>
-      <p>{params.quoteId}</p>
+      <HighlightedQuote text={quote.text} author={quote.author} />
       <Route path={`/quotes/${params.quoteId}/comments`}>
         <Comments />
       </Route>
