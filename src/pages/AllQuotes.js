87f9c778219a58e9e7a914ce3b699de5b5fab4ad
dochi486 <93mskim@gmail.com>
2022-09-27 17:13:02 +0900

    인용문 목록, 새 인용문 추가 양식 페이지 연결

--- src/pages/AllQuotes.js
+++ src/pages/AllQuotes.js
@@ -1,5 +1,20 @@
+import QuoteList from '../components/quotes/QuoteList';
+
+const DUMMY_QUOTES = [
+  {
+    id: 'q1',
+    author: '붱키',
+    text: '리액트는 재미없엉',
+  },
+  {
+    id: 'q2',
+    author: '준키',
+    text: '코딩은 재미없엉',
+  },
+];
+
 const AllQuotes = () => {
-  return <h1>All Quotes Page</h1>;
+  return <QuoteList quotes={DUMMY_QUOTES} />;
 };
 
 export default AllQuotes;
